79a4ffa8f39724a0584b8775d7f1f4b0dd3b4cf9
Ounaceur <abdeljalil.ounaceur2003@gmail.com>
2023-12-06 15:53:55 +0100

    Added the essential files

---
+++ links.js
@@ -0,0 +1,142 @@
+var data = [
+  {
+    "link_name": "Overleaf LaTeX editor",
+    "link": "https://www.overleaf.com/project/629b3f0a9dbffcdf1dc79868",
+    "color":"#FFA500"
+  },
+  {
+    "link_name": "Taha Website",
+    "link": "https://www.tahabouhsine.com/",
+    "color":"#0074D9"
+  },
+  {
+    "link_name": "Taha Blog",
+    "link": "https://www.tahabouhsine.com/blog/2022/09/09/courses/",
+    "color":"#0074D9"
+  },
+  {
+    "link_name": "Taha Links",
+    "link": "https://docs.google.com/document/d/1HcXyolF4TJEo-tgcCvtu0deXIkjPUK0J6j96RgI4I-k/edit",
+    "color":"#0074D9"
+  },
+  {
+    "link_name": "Statistics Visual Book",
+    "link": "https://seeing-theory.brown.edu/index.html",
+    "color":"#008000"
+  },
+  {
+    "link_name": "CS229br Foundations of Deep Learning",
+    "link": "https://boazbk.github.io/mltheoryseminar/",
+    "color":"#008000"
+  },
+  {
+    "link_name": "Math for Machine Learning",
+    "link": "https://www.cis.upenn.edu/~jean/math-deep.pdf",
+    "color":"#008000"
+  },
+  {
+    "link_name": "LoRA",
+    "link": "https://huggingface.co/docs/peft/conceptual_guides/lora",
+    "color":"#FFD700"
+  },
+  {
+    "link_name": "ResearchGate",
+    "link": "https://www.researchgate.net/",
+    "color":"#A2746C"
+  },
+  {
+    "link_name": "Arxiv",
+    "link": "https://arxiv.org/",
+    "color":"#A2746C"
+  },
+  {
+    "link_name": "ML in Depth - Taha Bouhsine",
+    "link": "https://www.youtube.com/watch?v=fqwOjLxrUcA&list=PLlqOXLg-GOqdzcWTwMIoUqyXx_MmXCNut&index=9",
+    "color":"#FF4136"
+  },
+  {
+    "link_name": "The art of ML in TensorFlow",
+    "link": "https://www.youtube.com/watch?v=3egPCCl7TgI",
+    "color":"#FF4136"
+  },
+  {
+    "link_name": "To ML or not ML",
+    "link": "https://www.youtube.com/watch?v=pz1lYngqXpc",
+    "color":"#FF4136"
+  },
+  {
+    "link_name": "Top AI Conferences Paper with Code",
+    "link": "https://github.com/MLNLP-World/Top-AI-Conferences-Paper-with-Code",
+    "color":"#111111"
+  },
+  {
+    "link_name": "Google Earth",
+    "link": "https://earthengine.google.com/",
+    "color":"#FFA500"
+  },
+  {
+    "link_name": "Zero-shot learning",
+    "link": "https://en.wikipedia.org/wiki/Zero-shot_learning",
+    "color":"#A2d46C"
+  },
+  {
+    "link_name": "Connected Papers",
+    "link": "https://www.connectedpapers.com/",
+    "color":"#A2746C"
+  },
+  {
+    "link_name": "MLOps",
+    "link": "https://www.wandb.courses/courses/effective-mlops-model-development",
+    "color":"#FFD700"
+  },
+  {
+    "link_name": "Udacity Data Visualization and D3.js",
+    "link": "https://www.udacity.com/course/data-visualization-and-d3js--ud507",
+    "color":"#008000"
+  },
+  {
+    "link_name": "EFSET English test online",
+    "link": "https://www.efset.org/",
+    "color":"#FFA500"
+  },
+  {
+    "link_name": "Duolingo English Test",
+    "link": "https://englishtest.duolingo.com/applicants",
+    "color":"#FFA500"
+  },
+  {
+    "link_name": "Diffusion Models",
+    "link": "https://lilianweng.github.io/posts/2021-07-11-diffusion-models/",
+    "color":"#FFD700"
+  },
+  {
+    "link_name": "GitHub INI7A",
+    "link": "https://github.com/skywolfmo/Ini7a",
+    "color":"#111111"
+  },
+  {
+    "link_name": "So Good They Can't Ignore You",
+    "link": "https://www.amazon.com/Good-They-Cant-Ignore-You/dp/1455509124",
+    "color":"#008000"
+  },
+  {
+    "link_name": "Dive into Deep Learning",
+    "link": "https://d2l.ai/index.html",
+    "color":"#008000"
+  },
+  {
+    "link_name": "Harvard CS197 Lecture Notes",
+    "link": "https://www.cs197.seas.harvard.edu/",
+    "color":"#008000"
+  },
+  {
+    "link_name": "Discore",
+    "link": "https://discord.gg/yZ2mUrWs",
+    "color":"#B10DC9"
+  },
+  {
+    "link_name": "What Nobody Tells You About Getting Rich",
+    "link": "https://www.youtube.com/watch?v=LcCtv9UkILI",
+    "color":"#FF4136"
+  }
+]
